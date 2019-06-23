9a069c20d2e46bf651447cfda781f5ba39bfa1c1
jkingz <jfkrule@gmail.com>
2019-06-24 03:41:13 +0800

    Create article detail endpoint

--- articles/urls.py
+++ articles/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import ArticleListView
+from .views import ArticleListView, ArticleDetailView
 
 
 urlpatterns = [
-    path('', ArticleListView.as_view({'get': 'list'})),
+    path('', ArticleListView.as_view(
+        {'get': 'list'}), name="article_list"),
+    path('', ArticleDetailView.as_view(
+        {'get': 'detail'}), name="article_detail"),
 ]
