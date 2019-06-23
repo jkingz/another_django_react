f9fb57d3a67934287d45f912341fb0b32407bd99
jkingz <jfkrule@gmail.com>
2019-06-24 03:34:24 +0800

    Include article endpoint to main app

--- djangoreact/urls.py
+++ djangoreact/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/', include('articles.urls')),
     path('admin/', admin.site.urls),
 ]
