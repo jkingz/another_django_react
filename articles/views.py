768e7191daee437e745590a04b2d358e294c098f
jkingz <jfkrule@gmail.com>
2019-06-24 03:31:04 +0800

    Create article list view

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from articles.models import Article
+from .serializers import ArticleSerializer
+
+
+# Article list
+class ArticleListView(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
