ddc3f10be57586ee03f2765cca31d37866915288
jkingz <jfkrule@gmail.com>
2019-06-24 05:11:59 +0800

    Created contrainers layout

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "antd/dist/antd.css";
 
-function App() {
+import CustomLayout from "./containers/Layout";
+import Article from "./components/Article";
+
+// es6 arrow function
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CustomLayout />
     </div>
   );
-}
+};
+
+// old way function base
+// function App() {
+//   return (
+//     <div className="App">
+//       <CustomLayout>
+//       </CustomLayout>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 export default App;
