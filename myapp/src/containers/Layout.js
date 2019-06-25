dc19acbf9bf7788653a3042dacfba7dee7eb1726
jkingz <jfkrule@gmail.com>
2019-06-25 22:45:36 +0800

    Added links to layout containers

--- myapp/src/containers/Layout.js
+++ myapp/src/containers/Layout.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import { Layout, Menu, Breadcrumb } from "antd";
 
 const { Header, Content, Footer } = Layout;
@@ -21,17 +23,18 @@ const CustomLayout = props => {
       </Header>
       <Content style={{ padding: "0 50px" }}>
         <Breadcrumb style={{ margin: "16px 0" }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
+          <Breadcrumb.Item>
+            <Link to="/">Home</Link>
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>
+            <Link to="/">List</Link>
+          </Breadcrumb.Item>
         </Breadcrumb>
         <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
           {props.children}
         </div>
       </Content>
-      <Footer style={{ textAlign: "center" }}>
-        Ant Design ©2018 Created by Ant UED
-      </Footer>
+      <Footer style={{ textAlign: "center" }}>©Copyright 2019</Footer>
     </Layout>
   );
 };
