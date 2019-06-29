a87e683f5a17b70f9aad8570f27f8abc5d8106ea
jkingz <jfkrule@gmail.com>
2019-06-29 22:49:55 +0800

    Created articles create api endpoint

--- articles/urls.py
+++ articles/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 
-from .views import ArticleListView, ArticleDetailView
+from .views import (
+    ArticleListView,
+    ArticleDetailView,
+    ArticleCreateView,
+)
 
 
 urlpatterns = [
     path('', ArticleListView.as_view(
         {'get': 'list'}), name="article_list"),
+    path(
+        'create/', ArticleCreateView.as_view({'post': 'create'}), name="create_article"),
     path('detail/<int:pk>/', ArticleDetailView.as_view(
         {'get': 'retrieve'}), name="article_detail"),
+
 ]
